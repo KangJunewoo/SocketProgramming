9476623f99bda10872c0d9139d13d981ac410aa1
KangJunewoo <brianjune@naver.com>
2020-05-30 13:40:00 +0900

    client-server 1단계 완료

---
+++ tcp_linux/file_transfer/client.c
@@ -0,0 +1,68 @@
+// server on
+// client on, access server and send "hello server"
+// server sends file to client by packet of 1460 bytes
+// client receives packets, storing in buffer, write file at last.
+// debug : compare contents, size of send file / receive file by cmp or diff.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char* message);
+
+int main(int argc, char* argv[]){
+  // fd 저장할 변수
+  int sock;
+
+  // serv_addr 저장할 주소
+  struct sockaddr_in serv_addr;
+  
+  // 이 메세지를 서버한테 보내는 게 목표!!
+  char message[]="hello world!";
+
+  // IP와 포트번호 제대로 입력해!
+  if(argc!=3){
+    printf("usage : %s <IP> <port>\n", argv[0]);
+    exit(1);
+  }
+
+  // 1단계 - 소켓생성
+  sock=socket(PF_INET, SOCK_STREAM, 0);
+  if(sock==-1)
+    error_handling("socket() error");
+
+  // 서버주소 터미널에서 입력한대로 설정
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family=AF_INET;
+  serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+  serv_addr.sin_port=htons(atoi(argv[2]));
+
+
+  /**
+   * 2단계 - 연결요청
+   * 성공시 0, 실패시 -1 리턴.
+   * 인자로 fd, 서버주소값, 서버주소크기 들어감.
+  */
+  if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+    error_handling("connect() error");
+
+  /**
+   * 3단계 - 데이터 송수신
+   * write : client fd, msg to send, msg크기 (for server)
+   * read : client fd, msg to be saved, msg크기 (for client)
+  */
+  write(sock, message, sizeof(message));
+  
+  // 4단계 - 연결 종료.
+  close(sock);
+  return 0;
+}
+
+void error_handling(char* message){
+  fputs(message,stderr);
+  fputc('\n',stderr);
+  exit(1);
+}
---
+++ tcp_linux/file_transfer/server.c
@@ -0,0 +1,107 @@
+// server on
+// client on, access server and send "hello server"
+// server sends file to client by packet of 1460 bytes
+// client receives packets, storing in buffer, write file at last.
+// debug : compare contents, size of send file / receive file by cmp or diff.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char *message);
+
+int main(int argc, char* argv[]){
+  int serv_sock;
+  int clnt_sock;
+
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in clnt_addr;
+
+  socklen_t clnt_addr_size;
+
+  // 메세지 버퍼와 그걸 담을 크기.
+  char message[30];
+  int str_len;
+
+  if(argc!=2){
+    printf("usage : %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  /**
+   * 1단계 - 소켓생성
+   * 성공 시 fd, 실패시 -1 반환
+   * 
+   * 인자1 PF_INET : ipv4에서 돌리겠다.
+   * 인자2,3 : TCP or UDP 결정.
+   * TCP : SOCK_STREAM, IPPROTO_TCP
+   * UDP : SOCK_DGRAM, IPPROTO_UDP
+  */
+
+  serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+  if(serv_sock==-1)
+    error_handling("socket() error");
+
+  /**
+   * memset으로 serv_addr를 0으로 싹 초기화해주고
+   * 주소체계는 ipv4로
+   * ip주소 세팅 : host_order to network_order, for long. 자기 IP주소를 네트워크 바이트 형태로 바꿔 할당.
+   * 포트번호 세팅 : host_order to network_order, for short. argument를 int로 바꾸고 네트워크 바이트 형태로 바꿔 할당.
+  */
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family=AF_INET;
+  serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+  serv_addr.sin_port=htons(atoi(argv[1]));
+
+  /**
+   * 2단계 - 소켓 주소 할당
+   * 성공시 0, 실패시 -1 리턴.
+   * 인자로 fd, 서버주소, 주소값의 길이정보 들어감.
+  */
+
+  if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+    error_handling("bind() error");
+
+  if(listen(serv_sock, 5)==-1)
+    error_handling("listen() error");
+
+  clnt_addr_size=sizeof(clnt_addr);
+
+  /**
+   * 4단계 - 연결 허용 이제 진짜 연결되어서 clnt_addr 활성화됨.
+   * 성공시 fd, 실패시 -1 반환.
+   * 인자로 client fd, client 주소, 주소크기 들어감.
+  */
+  clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+  if(clnt_sock==-1)
+    error_handling("accept() error");
+
+  /**
+   * 5단계 - 데이터 송수신
+   * write : client fd, msg to send, msg크기 (for server)
+   * read : client fd, msgto be saved, msg크기 (for client)
+  */
+  str_len=read(clnt_sock, message, sizeof(message)-1);
+  if(str_len==-1)
+    error_handling("read() error");
+  printf("message from client:%s\n", message);
+  
+  /**
+   * 6단계 - 연결 종료
+   * 성공시 0, 실패시 -1 리턴.
+   * 인자로 fd 들어감.
+  */
+  close(clnt_sock);
+  close(serv_sock);
+  return 0;
+}
+
+void error_handling(char* message){
+  fputs(message, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
