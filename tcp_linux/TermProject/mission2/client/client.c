6b5cb97e190cbc20211f2d5671ee02b24a56f246
KangJunewoo <brianjune@naver.com>
2020-06-19 09:53:25 +0900

    mission 2 complete

--- tcp_linux/TermProject/mission2/client/client.c
+++ tcp_linux/TermProject/mission2/client/client.c
@@ -12,12 +12,13 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define MAX_LINE 1460
+// 버퍼사이즈 선언. 1460바이트씩 보낸다.
+#define BUFF_SIZE 1460
 
 // 에러핸들링을 위한 함수
 void error_handling(char* message);
 
-// 파일을 쓰는 함수
+// 서버한테서 받은 파일을 write하는 함수
 void writefile(int sock, FILE *fp);
 
 // 전체 쓰여진 바이트 수
@@ -28,10 +29,10 @@ int main(int argc, char* argv[]){
   int sock;
   struct sockaddr_in serv_addr;
   
-  // 메세지를 저장할 버퍼 선언, Hello Server로 초기화.
+  // 메세지와 파일이름을 저장할 버퍼 선언.
+  // 처음에 보낼 메세지인 "Hello Server!"로 초기화.
   char message[30]="Hello Server!";
   char filename[30];
-  int str_len;
 
   // IP와 포트번호 제대로 입력되지 않은 경우 에러처리.
   if(argc!=3){
@@ -40,7 +41,6 @@ int main(int argc, char* argv[]){
   }
 
   // ****** 1단계 - socket() 함수를 통한 소켓생성 ******
-  
   // IPV4, TCP를 사용함
   sock=socket(PF_INET, SOCK_STREAM, 0);
   if(sock==-1)
@@ -54,29 +54,27 @@ int main(int argc, char* argv[]){
 
 
   // ****** 2단계 - connect() 함수를 통한 연결 요청 ******
-
   if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
     error_handling("connect() error");
 
   // ****** 3단계 - 데이터 송수신 ******
-
-  // 서버한테 메세지 전송 후
+  // 서버한테 Hello Server 전송 후
   printf("Sending \"%s\" to server..\n", message);
-  write(sock, message, sizeof(message));
+  write(sock, message, strlen(message)+1);
   
-  // 서버한테서 파일이름을 받아 파일이름으로 저장.
-  str_len=read(sock, message, sizeof(message));
-  
-  //write(sock, NULL, MAX_LINE);
-  if(str_len==-1)
+  // 서버한테서 파일이름을 받아 저장.
+  if(read(sock, message, sizeof(message))==-1)
     error_handling("read() error");
-  printf("Filename : %s\n", message);
+  printf("Filename : %s\n\n", message);
   strcpy(filename, message);
 
-  // 파일을 열고 writefile 함수를 통해 서버로부터 받은 파일데이터 저장.
+  // 파일을 열고
   FILE *fp = fopen(filename, "wb");
   if(fp==NULL)
     error_handling("Cannot open file");
+  
+  // writefile 함수를 통해 파일데이터 저장.
+  printf("Downloading in progress...\n");
   writefile(sock, fp);
   printf("Receive Success, %ld bytes received.\n", total);
   
@@ -98,23 +96,21 @@ void error_handling(char* message){
 }
 
 void writefile(int sock, FILE *fp){
-  // 읽어들인 바이트를 담을 변수와 크기 1460의 버퍼 선언
+  // 읽어들인 바이트를 담을 변수와 버퍼 선언
   ssize_t bytes_read;
-  char buff[MAX_LINE]={0};
-  // 1460바이트만큼 계속 읽어들이며 buff에 저장
-  while((bytes_read=read(sock,buff,MAX_LINE))>0){
-    //printf("%ld ", bytes_read);
-    // total에 읽어들인 바이트만큼 채우고, 실패시 에러처리.
+  char buff[BUFF_SIZE]={0};
+
+  // 1460바이트만큼 계속 읽어들이며 버퍼에 저장
+  while((bytes_read=read(sock,buff,BUFF_SIZE))>0){
     total+=bytes_read;
     if(bytes_read==-1)
       error_handling("Receiving Error");
 
-    // buff의 데이터를 파일에 write하고, 실패시 에러처리.
-    if(fwrite(buff, sizeof(char), bytes_read, fp)==-1){
+    // 읽기 완료된 버퍼의 데이터를 파일에 write함.
+    if(fwrite(buff, sizeof(char), bytes_read, fp)==-1)
       error_handling("File-Writing Error");
-    }
 
     // 다음 데이터를 읽기 전 버퍼 초기화
-    memset(buff, 0, MAX_LINE);
+    memset(buff, 0, BUFF_SIZE);
   }
 }
--- tcp_linux/TermProject/mission2/server/server.c
+++ tcp_linux/TermProject/mission2/server/server.c
@@ -12,15 +12,16 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define MAX_LINE 1460
+// 버퍼사이즈 선언. 1460바이트씩 보낸다.
+#define BUFF_SIZE 1460
 
 // 에러핸들링을 위한 함수
 void error_handling(char *message);
 
-// 파일을 보내기 위한 함수
+// 클라이언트한테 파일을 보내기 위한 함수
 void sendfile(int sock, FILE* fp);
 
-// 총 보낸 바이트 수
+// 전체 보낸 바이트 수
 ssize_t total=0;
 
 int main(int argc, char* argv[]){
@@ -35,19 +36,17 @@ int main(int argc, char* argv[]){
   // 클라이언트 주소 크기
   socklen_t clnt_addr_size;
 
-  // 메세지를 담을 버퍼. 여기 담긴 메세지를 클라이언트한테 보내는 것이 목표.
+  // 메세지와 파일이름을 저장할 버퍼 선언.
   char message[30];
   char filename[30];
-  int str_len;
 
-  // 만약 포트번호가 입력되지 않았다면 에러처리.
+  // 포트번호가 입력되지 않았다면 에러처리.
   if(argc!=2){
     printf("usage : %s <port>\n", argv[0]);
     exit(1);
   }
 
   // ****** 1단계 - socket() 함수를 통한 소켓생성 ******
-
   // IPV4, TCP를 사용함.
   serv_sock=socket(PF_INET, SOCK_STREAM, 0);
   if(serv_sock==-1)
@@ -61,46 +60,42 @@ int main(int argc, char* argv[]){
   serv_addr.sin_port=htons(atoi(argv[1]));
 
   // ****** 2단계 - bind() 함수를 통한 소켓 주소 할당 ******
-
   if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
     error_handling("bind() error");
 
   // ****** 3단계 - listen() 함수를 통한 연결요청 대기상태 진입 ******
-
+  // 연결요청 queue size는 5로 잡음.
   if(listen(serv_sock, 5)==-1)
     error_handling("listen() error");
 
+  // 클라이언트 주소크기 저장
   clnt_addr_size=sizeof(clnt_addr);
 
   // ****** 4단계 - accept() 함수를 통한 연결 허용 ******
-
   clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
   if(clnt_sock==-1)
     error_handling("accept() error");
 
   // ****** 5단계 - 데이터 송수신 ******
-  
-  // 클라이언트한테서 메세지를 읽고 에러가 없다면 print
-  str_len=read(clnt_sock, message, sizeof(message));
-  if(str_len==-1)
+  // 클라이언트한테서 메세지를 받아 출력
+  if(read(clnt_sock, message, sizeof(message))==-1)
     error_handling("read() error");
   printf("Message from client : %s\n", message);
 
-  // 그 뒤 파일이름을 입력받은 후 클라이언트에게 파일 이름 전송
+  // 파일이름을 입력받고, 클라이언트에게 파일 이름 전송
   printf("Enter filename to send : ");
   scanf("%s", filename);
-
-
-  // 파일을 열고 sendfile 함수를 통해 해당 파일 전송.
-  FILE *fp = fopen(filename, "rb");
-  if(fp==NULL)
-    error_handling("Cannot Open File");
-
-  
   strcpy(message, filename);
   if(write(clnt_sock, message, sizeof(message))==-1)
     error_handling("cannot send filename");
 
+  // 파일을 열고
+  FILE *fp = fopen(filename, "rb");
+  if(fp==NULL)
+    error_handling("Cannot Open File");
+  
+  // sendfile 함수를 통해 파일데이터 전송.
+  printf("Sending in progress...\n");
   sendfile(clnt_sock, fp);
   printf("Send Success, %ld bytes send\n", total);
   
@@ -120,26 +115,24 @@ void error_handling(char* message){
   fputc('\n', stderr);
   exit(1);
 }
-// TODO : progress bar 할만할듯
-void sendfile(int sock, FILE* fp){
-  // 읽어들인 바이트를 담을 변수와 크기 1460의 버퍼 선언
-  ssize_t bytes_read;
-  char buff[MAX_LINE]={0};
 
-  // 1460바이트만큼 파일을 계속 읽어서 버퍼에 저장
-  while((bytes_read=fread(buff, sizeof(char), MAX_LINE, fp))>0){
-    //printf("%ld ", bytes_read);
-    // total에 읽어들인 바이트만큼 채우고, 실패시 에러처리.
+void sendfile(int sock, FILE* fp){
+  // 읽어들인 바이트를 담을 변수와 버퍼 선언
+  ssize_t bytes_read;
+  char buff[BUFF_SIZE]={0};
+
+  // 1460바이트만큼 계속 읽어들이며 버퍼에 저장
+  while((bytes_read=fread(buff, sizeof(char), BUFF_SIZE, fp))>0){
     total+=bytes_read;
-    if(bytes_read!=MAX_LINE&&ferror(fp)){
+    if(bytes_read!=BUFF_SIZE&&ferror(fp)){
       error_handling("File-Read Error");
     }
 
-    // buff의 데이터를 소켓에 write하고, 실패시 에러처리.
+    // 읽기 완료된 버퍼의 데이터를 소켓을 통해 전송.
     if(write(sock, buff, bytes_read)==-1)
       error_handling("Cannot send file");
 
     // 다음 데이터를 읽기 전 버퍼 초기화
-    memset(buff, 0, MAX_LINE);
+    memset(buff, 0, BUFF_SIZE);
   }
 }
